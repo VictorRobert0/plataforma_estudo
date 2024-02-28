4595f12c64911b8a5834118236ae08bb32b10484
VictorRobert0 <contatokataltd@gmail.com>
2024-02-27 21:39:12 -0300

    primeiro commit

---
+++ usuarios/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path ('cadastro/', views.cadastro, name="cadastro"),
+    path('login/', views.login, name= "login"),
+    path('seguranca/', views.seguranca),
+
+    
+]
+
+
